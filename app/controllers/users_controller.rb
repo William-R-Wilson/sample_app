964c1de3706588a90f9f9419999b9ff07d9e0da7
Will <williamrwilson@gmail.com>
2015-02-21 00:46:57 +0000

    add resources and view for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def new
   end
+  
+  def show
+    @user = User.find(params[:id])
+    # debugger = this line activates byebug, enables the use of a console type environment 
+    # in the server
+  end
+  
 end
