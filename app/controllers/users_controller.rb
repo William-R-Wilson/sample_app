b3278081aa03041c528106c7b9e5f82f9fcd3855
Will <williamrwilson@gmail.com>
2015-02-22 00:49:30 +0000

    finish 8.4.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,17 @@
 class UsersController < ApplicationController
+  
   def new
     @user = User.new
   end
   
   def show
     @user = User.find(params[:id])
-    # debugger = this line activates byebug, enables the use of a console type environment 
-    # in the server
   end
   
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the sample app!"
       redirect_to @user
     else
